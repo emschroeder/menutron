0a01d466f0b1daac3b3437f8a8cb84bdfc1f7710
Schroeder <eschroeder@engr-134-19.eastern.ewu.edu>
2014-05-21 14:16:49 -0700

    menutron

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+  $('#main').menutron({
+  	menuTitle: "Eat Sushi!!"
+  });
+
+});
+
+//From https://github.com/micjamking/Menutron
